477af99b99d8810391cc0084f073b7e976541fb3
krishnarajesh612 <44587450+krishnarajesh612@users.noreply.github.com>
2018-10-30 06:22:10 +0530

    Add files via upload

---
+++ demo7.py
@@ -0,0 +1,10 @@
+while True:
+    name = input('Enter your Name:  ')
+    if name != 'testuser' or name != 'TESTUSER'
+                 continue
+                 print('Welcome user ' + name)
+                 password= input('Enter your password please. it\' is fishname')
+                 if password =='bluefish':
+                 break
+
+print('Access is granted...')
